31c05fbfcf23fddf9dbde9e316a1ecb26d522723
dsandelc <diegosanchezd@hotmail.com>
2022-05-17 08:38:38 +0200

    Terminado - Para revisar

--- index.js
+++ index.js
@@ -1,63 +1,44 @@
+"use strict";
 // En este ejercicio debéis crear una clase "Person" con las propiedades privadas "name" y "age" y la propiedad pública "saySomething". Añadid también el método "myNameIs" para que saque por consola el valor de la propidad "name".
 //En su constructor, debéis dar un valor a cada propiedad (sin contar "myNameIs") teniendo claro que sus tipos son cadena de texto, número y función que recibe cadena y no devuelve nada, respectivamente.
 // Luego, cread una clase "Developer" que herede de "Person" y que tenga la propiedad pública "bestLanguage". El tipo de la propiedad "bestLanguage" es función que recibe cadena y no devuelve nada.
 // En el constructor de "Developer", debéis añadir los parámetros al constructor de la clase padre (super) y setear la propiedad "bestLanguage" sabiendo que es de tipo función que recibe una cadena.
 // Cuando acabéis, debéis descomentar las siguientes líneas de código y colocarlas despúes de todo lo que añadáis:
-
 // const developer = new Developer(
 //   "pedro",
 //   35,
 //   something => console.log(something),
 //   language => console.log(language + " is the best language")
 // );
-
 // developer.myNameIs();
 // developer.saySomething('algo');
 // developer.bestLanguage('Javascript');
-
 //--------- PUNTO 1 Y 2 ----------//
-
 // Creo la clase persona
 class Person {
-  /* Atributos */
-  name: string;
-  age: number;
-  public saySomething: any;
-  /* Constructor */
-  constructor(nombre: string, edad: number, say: any) {
-    this.name = nombre;
-    this.age = edad;
-    this.saySomething = say;
-  }
-  /* Métodos */
-  myNameIs() {
-    console.log(this.name);
-  }
+    /* Constructor */
+    constructor(nombre, edad, say) {
+        this.name = nombre;
+        this.age = edad;
+        this.saySomething = say;
+    }
+    /* Métodos */
+    myNameIs() {
+        console.log(this.name);
+    }
 }
-
 //--------- PUNTO 3 Y 4 ----------//
-
 class Developer extends Person {
-  /* Atributos */
-  public bestLanguage: string;
-  /* Constructor */
-  constructor(nombre: string, edad: number, say: any, lenguaje: any) {
-    super(nombre, edad, say);
-    this.bestLanguage = lenguaje;
-  }
-  /* Método */
-  set() {}
+    /* Constructor */
+    constructor(nombre, edad, say, lenguaje) {
+        super(nombre, edad, say);
+        this.bestLanguage = lenguaje;
+    }
+    /* Método */
+    set() { }
 }
-
 //--------- PUNTO 5 ----------//
-
-const developer = new Developer(
-  'pedro',
-  35,
-  (something) => console.log(something),
-  (language) => console.log(language + ' is the best language')
-);
-
+const developer = new Developer('pedro', 35, (something) => console.log(something), (language) => console.log(language + ' is the best language'));
 developer.myNameIs();
 developer.saySomething('algo');
 //developer.bestLanguage('Javascript'); 
